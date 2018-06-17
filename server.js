9e68ad4887db9bf95d5973dbd7663a62552237d3
AgentGroda <31662028+AgentGroda@users.noreply.github.com>
2018-06-17 18:26:11 +0200

    Clean up.
    Removed unnecessary thing.

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-/*
-    The following code goes into it's own file, and you run this file
-    instead of your main bot file.
-*/
-const http  = require('http')
-
-const express = require('express')
-const app = express()
-
+//Sharding
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./server1.js');
 Manager.spawn(2);
-
-  
-
-
-//app.get('/test', './views/index.html')
