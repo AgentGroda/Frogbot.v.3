8a72d26d41f6604a2761c3775b4732f291abb1e5
AgentGroda <31662028+AgentGroda@users.noreply.github.com>
2018-06-18 11:53:22 +0200

    Fixed user search

--- commands/id.js
+++ commands/id.js
@@ -1,22 +1,27 @@
+const Discord = require('discord.js')
+const sm = require('string-similarity');
+
 module.exports.run = async (client, message, args) => {
+  let members = [];
+  let indexes = [];
   
-try {
-
-    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-
-    if (user) return message.channel.send(`Here is the id for **${user.user.username}**: **${user.id}**`)
-    else {
-    message.channel.send(`Here is the id for **${message.author.username}**: **${message.author.id}**`)
-    }
-} catch (err) {
-    message.channel.send(`Error: ${err.name}: **${err.message}**`)
+  message.guild.members.forEach(function(member){
+    members.push(member.user.username);
+    indexes.push(member.id);
+  })
+  let match = sm.findBestMatch(args.join(' '), members);
+  let username = match.bestMatch.target;
+  let member = message.guild.members.get(indexes[members.indexOf(username)])
+  let usernameUser = '';
+  let UserMention = '';
+  
+  if(!args[0]) {
+    usernameUser = message.author
+    UserMention = message.member
+  } else {
+  let mention = message.mentions.users.first()
+    usernameUser = mention || member.user
+    UserMention = message.mentions.members.first() || message.guild.members.get(args[0]) || member
+  }
+  message.channel.send(`**Here is ID for: ${usernameUser.username}: ${usernameUser.id}**`)
 }
-
-}
-exports.conf = {
-    aliases: ['example', 'chexample']
-    };
-   
-    exports.help = {
-    name: 'example', description: 'hello this is description', usage: 'example'
-    };
