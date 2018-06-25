fd92fa7d6eda57ab5abedf83b6934f6351c00d8b
AgentGroda <31662028+AgentGroda@users.noreply.github.com>
2018-06-25 12:33:32 +0200

    Fixed Mod-Log

--- commands/mute.js
+++ commands/mute.js
@@ -18,9 +18,9 @@ module.exports.run = async (client, message, args) => {
         name: 'Mute Error',
         icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
     })
-   // let muterole = message.guild.roles.find(`name`, "Muted");
+
     let muterole = message.guild.roles.find(role => role.name === 'Muted');
-    //let modlog = message.guild.channels.find(`name`, "mod-log")
+
 
 
     const mod = message.author;
@@ -29,19 +29,9 @@ module.exports.run = async (client, message, args) => {
     const a = guildcasenumber
     const b = a+1
     casenumbers.set(`case_${message.guild.id}`, b)
-  //  console.log(guildcasenumber)
-  let log = db.fetch(`modlog__${message.guild.id}`)
 
-  if (!message.guild.channels.get(log)) return message.channel.send('**Plese set a mod-log channel using =setmodlog**')
-  const modlog = message.guild.channels.get(log)
+const modlog = message.guild.channels.find(channel => channel.name === 'mod-log');
 
-    console.log(log)
-    console.log(typeof log)
-
-
-
-
-  //  console.log(modlog)
     
   
 
@@ -72,7 +62,7 @@ module.exports.run = async (client, message, args) => {
         icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
     });
   
-    //let reason = args[2];
+
       let reason = message.content.split(" ").slice(3).join(" ");
     if (!reason) return send(message.channel, "You didn't specify a reason!", {
         name: 'Mute Error',
@@ -81,7 +71,7 @@ module.exports.run = async (client, message, args) => {
 
     await (tomute.roles.add(muterole.id));
 
-   //modlog.send(`${tomute} has been muted for ${ms(ms(mutetime))} for the reason ${reason}`)
+
 
 
     const embed = new Discord.MessageEmbed()
@@ -132,10 +122,69 @@ module.exports.run = async (client, message, args) => {
     }, ms(mutetime));
 
 }
-exports.conf = {
-    aliases: ['example', 'chexample']
-    };
-   
-    exports.help = {
-    name: 'example', description: 'hello this is description', usage: 'example'
-    };
+
+    let mutetime = args[1];
+    if (!mutetime) return send(message.channel, "You didn't specify a time!", {
+        name: 'Mute Error',
+        icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
+    });
+  
+
+      let reason = message.content.split(" ").slice(3).join(" ");
+    if (!reason) return send(message.channel, "You didn't specify a reason!", {
+        name: 'Mute Error',
+        icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
+    })
+
+    await (tomute.roles.add(muterole.id));
+
+
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor('Mute')
+    .addField('Moderator', `${mod}`)
+    .addField('User', `<@${tomute.id}>`)
+    .addField('Time', `${ms(ms(mutetime))}`)
+    .addField('Reason', `${reason}`)
+    .addField('Case Number', `${guildcasenumber}`)
+    .setColor('BLUE')
+    .setTimestamp()
+    .setThumbnail(tomute.user.avatarURL())
+    .setFooter(`ID ${tomute.id}`)
+    send(modlog, embed, {
+        name: 'Mute',
+        icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
+    })
+    send(message.channel, 'I have muted the person and logged it!', {
+        name: 'Mute',
+        icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
+    })
+
+    const guildcasenumber1 = await casenumbers.fetch(`case_${message.guild.id}`)
+    const c = guildcasenumber1
+    const d = c+1
+    casenumbers.set(`case_${message.guild.id}`, d)
+    
+    
+
+    setTimeout(function() {
+
+        tomute.roles.remove(muterole.id);
+
+
+      const embed1 = new Discord.MessageEmbed()
+      .setAuthor('Unmute')
+      .addField('Moderator', `<@364071140034871296>`)
+      .addField('User', `<@${tomute.id}>`)
+      .addField('Reason', `Auto`)
+      .addField('Case Number', `${guildcasenumber1}`)
+      embed1.setColor('BLUE')
+      embed1.setTimestamp()
+      embed1.setFooter(`ID ${tomute.id}`)
+      send(modlog, embed1, {
+          name: 'Mute',
+          icon: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/denied-512.png'
+      })
+    }, ms(mutetime));
+
+}
