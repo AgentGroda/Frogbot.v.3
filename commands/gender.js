ae8a8a79e690e5d172508569e632e24854205856
AgentGroda <31662028+AgentGroda@users.noreply.github.com>
2018-06-25 12:15:46 +0200

    Set own gender.

--- commands/gender.js
+++ commands/gender.js
@@ -1,38 +1,24 @@
 const db = require('quick.db')
 module.exports.run = async (client, message, args) => {
 
-    if (!args[0]) return message.channel.send('**Please input a gender. `female, male or nonbinary`**')
-    //const d = ['male', 'female']
-    //if (!args === 'male') return message.channel.send('**You can only use `female or male`**')
+    if (!args[0]) message.channel.send('**Please input a valid option.** `set` **or** `view`')
+
 
     const gender = new db.table('genders')
 
-    if (args[0] === 'female') {
-       // let data1 = { gender: 'Female'};
-        gender.set(`genders_${message.author.id}`, 1)
-        message.channel.send('**Gender set to female!**')
+    if (args[0] === 'set') {
+        if (!args[1]) return message.channel.send('**Please input a gender**')
+        gender.set(`genders_${message.author.id}`, args[1])
+        message.channel.send(`**Gender set to ${args[1]}**`)
     }
-    if (args[0] === 'male') {
-       // let data2 = { gender: 'Male'};
-        gender.set(`genders_${message.author.id}`, 2)
-        message.channel.send('**Gender set to male!**')
 
-
-    }
-    if (args[0] === 'view') {
-
-        let genders = new db.table('genders');
-        genders.fetch(`genders_${message.author.id}`).then(obj => {
-          if (obj === null) obj = 'Not set.';
-          if (obj === 1) obj = 'Female';
-          if (obj === 2) obj = 'Male';
-          if (obj === 3) obj = 'Nonbinary';
-        
-        message.channel.send(`**Gender:** \`${obj}\``)
+if (args[0] === 'view') {
+    gender.fetch(`genders_${message.author.id}`).then(o => {
+        if (o === null) o = 'Not Set.'
+        message.channel.send(`**Your gender is: ${o}**`)
     })
-    }
-    if (args[0] === 'nonbinary') {
-        gender.set(`genders_${message.author.id}`, 3)
-        message.channel.send('**Gender set to nonbinary!**')
-    }
+}
+    
+
+ 
 }
