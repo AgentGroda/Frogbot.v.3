1a0a1ebde2e475f069cd33982b2f6c8ea69e1037
AgentGroda <31662028+AgentGroda@users.noreply.github.com>
2018-06-25 12:24:15 +0200

    Fixed

--- commands/todo.js
+++ commands/todo.js
@@ -1,7 +1,10 @@
 module.exports.run = async (client, message, args) => {
-  if (!message.author.id === '295231913612804096') message.channel.send('**This command is only for the bot owner!**')
+  if (message.author.id !== '295231913612804096') return message.channel.send('**This command is only for the bot owner!**');
+
     let data = message.content.split(" ").slice(1).join(" ");
+    if (!data) return message.channel.send('Please enter a new todo!')
   
     client.channels.get('436897850270547978').send(`**New Todo!** ${data}`)
-  
+
 }
+
